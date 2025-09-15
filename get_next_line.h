4fe9ad3ef8beca637f55eb06dbec625cfc6e1bb7
Saifudin Mamaev <saifmamaev720@gmail.com>
2025-09-15 23:11:56 +0400

    new_one

--- get_next_line.h
+++ get_next_line.h
@@ -6,28 +6,17 @@
 /*   By: samamaev <samamaev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/30 18:34:36 by samamaev          #+#    #+#             */
-/*   Updated: 2025/09/07 19:41:52 by samamaev         ###   ########.fr       */
+/*   Updated: 2025/09/10 18:49:00 by samamaev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE
 # define GET_NEXT_LINE
 
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 42
-#endif
-
-# include <unistd.h>
-# include <stdlib.h>
-
-typedef struct s_list
+typedef struct get_next_line
 {
-	int				fd;
-	char			*buffer;
-	struct s_list	*next;
-}	t_list;
-char	*get_next_line(int fd);
-t_list *find_or_create_node(t_list **head, int fd);
-int		reading(t_list *node, int fd);
+    char *content;
+    struct s_list *node;
+}; t_list
 
 #endif
