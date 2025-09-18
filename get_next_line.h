a9c92b273e690ae78e15ce09b876a08f10749179
Saifudin Mamaev <saifmamaev720@gmail.com>
2025-09-18 23:34:42 +0400

    update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -6,17 +6,26 @@
 /*   By: samamaev <samamaev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/30 18:34:36 by samamaev          #+#    #+#             */
-/*   Updated: 2025/09/10 18:49:00 by samamaev         ###   ########.fr       */
+/*   Updated: 2025/09/18 22:07:55 by samamaev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE
 # define GET_NEXT_LINE
 
-typedef struct get_next_line
-{
-    char *content;
-    struct s_list *node;
-}; t_list
+# include <unistd.h>
+# include <stdlib.h>
 
+
+typedef struct s_list
+{
+    char*			content;
+	struct t_list	*next;
+} t_list;
+
+ typedef struct s_newline_pos
+ {
+    struct t_list	*node;
+    int				position;
+ } t_newline_pos;
 #endif
